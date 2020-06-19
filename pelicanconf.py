8ad0c20e22da9cd01abf3d5380b0f59e3726cdeb
Callum Rollo <c.rollo@outlook.com>
2020-06-19 21:44:13 +0100

    fixing home link

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
 
 AUTHOR = 'Callum Rollo'
+SITEURL = 'https://callumrollo.github.io'
 SITENAME = 'Callum Rollo'
-SITETITLE = AUTHOR
+SITETITLE = 'Callum Rollo'
 SITESUBTITLE = 'Oceanographer'
 SITEDESCRIPTION = "Science should be open. Software should be free. Borders are kind of rubbish "
-SITEURL = ''
 SITELOGO = SITEURL + '/images/mug.jpg'
 FAVICON = SITEURL + '/images/favicon.ico'
+BROWSER_COLOR = '#333333'
+PYGMENTS_STYLE = 'monokai'
 
 ROBOTS = 'index, follow'
 
-# Creative Commons Licensing
-CC_LICENSE = {
-        'name': 'Creative Commons Attribution-ShareAlike',
-        'version': '4.0',
-        'slug': 'by-sa'
-}
-COPYRIGHT_YEAR = 2020
-
-
-THEME = '/media/callum/storage/Documents/foo/Flex'
+THEME = '../Flex'
 PATH = 'content'
-STATIC_PATHS = ['images', 'figures']
-ARTICLE_PATHS = ['articles']
-PAGE_PATHS = ['pages']
+OUTPUT_PATH = 'blog/'
 TIMEZONE = 'Europe/London'
 DEFAULT_LANG = 'en'
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -37,19 +30,30 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
 
-
-
-
-# Social widget
 SOCIAL = (
 	('twitter', 	'https://twitter.com/callum_rollo'),
 	 ('github', 	'https://github.com/callumrollo'),
 	)
 
+MENUITEMS = (('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
 
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
 
+COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
+
+
+
+STATIC_PATHS = ['images', 'figures']
 
 
 
